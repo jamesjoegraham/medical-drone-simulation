8efa9af12421dff4d612e591a5aef9f82630b2b1
Kory Melchior <kory.melchior@dal.ca>
2022-11-02 13:33:32 -0300

    Changed loading of json file to include current directory of file since it wasn't working for me.
    added feature so that code waits for publisher to connect with subscribers before sending topic comman/pose. it is working now.
    
    still need to add something to check when quadrotor is within range of waypoint to start next one.

--- waypoint_control/waypoint_navigation.py
+++ waypoint_control/waypoint_navigation.py
@@ -4,13 +4,14 @@ import rospy
 from geometry_msgs.msg import Twist, PoseStamped, Pose
 from gazebo_msgs.msg import LinkStates
 import json
+import os
 
 #cur_pose = Pose()
 #def gazebo_pose_callback(msg):
 #    cur_pose = msg.pose
 
 # Open Waypoints JSON File
-waypoint_file = open("waypoints_1.json")
+waypoint_file = open(os.path.dirname(__file__)+ '/waypoints_1.json')
 waypoints = json.load(waypoint_file)
 
 # Initialize ROS Functionality
@@ -31,6 +32,11 @@ for waypoint in waypoints:
 
     print(pose_msg)
 
+    
+    #wait for connections before publishing messsage
+    while(pose_pub.get_num_connections() < 1):
+        pass
+    
     pose_pub.publish(pose_msg)
     rospy.spin()
 
