fbe4e249fc0b1d81ebf3f45250024f2f636b82d4
James Graham <james.joe.graham@gmail.com>
2022-11-10 16:29:52 -0400

    Added waiting for next waypoint to waypoint control  - sigint handler  - multiple waypoints  - added second waypoint to waypoints file

--- waypoint_control/waypoint_navigation.py
+++ waypoint_control/waypoint_navigation.py
@@ -2,13 +2,34 @@
 
 import rospy
 from geometry_msgs.msg import Twist, PoseStamped, Pose
-from gazebo_msgs.msg import LinkStates
 import json
 import os
+import sys, signal
 
-#cur_pose = Pose()
-#def gazebo_pose_callback(msg):
-#    cur_pose = msg.pose
+# Used to allow for CTRL-C (SIGINT) to exit when in while loop
+def signal_handler(signal, frame):
+    print("\nProgram Exiting")
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+cur_pose = Pose()
+def gazebo_pose_callback(msg):
+    global cur_pose
+    cur_pose = msg.pose
+
+def arrived_at_waypoint(current, waypoint):
+    dx = abs(waypoint.position.x - current.position.x)
+    dy = abs(waypoint.position.y - current.position.y)
+    dz = abs(waypoint.position.z - current.position.z)
+
+    close_enough = 0.5
+
+    # If we're in this range
+    if dx < close_enough and dy < close_enough and dz < close_enough:
+        return True
+
+    return False
+
 
 # Open Waypoints JSON File
 waypoint_file = open(os.path.dirname(__file__)+ '/waypoints_1.json')
@@ -17,7 +38,11 @@ waypoints = json.load(waypoint_file)
 # Initialize ROS Functionality
 rospy.init_node('waypoint_manager')
 pose_pub = rospy.Publisher('/command/pose', PoseStamped, queue_size=1)
-#rospy.Subscriber("gazebo/link_states", LinkStates, gazebo_pose_callback)
+rospy.Subscriber("/ground_truth_to_tf/pose", PoseStamped, gazebo_pose_callback)
+
+# Wait for connections before publishing messsage
+while(pose_pub.get_num_connections() < 1):
+    pass
 
 for waypoint in waypoints:
     print("Moving to waypoint")
@@ -30,16 +55,13 @@ for waypoint in waypoints:
     pose_msg = PoseStamped()
     pose_msg.pose = pose
 
-    print(pose_msg)
-
-    
-    #wait for connections before publishing messsage
-    while(pose_pub.get_num_connections() < 1):
-        pass
-    
     pose_pub.publish(pose_msg)
-    rospy.spin()
 
     # Wait until waypoint has been reached
+    while(not arrived_at_waypoint(cur_pose, pose_msg.pose)):
+        pass
 
-rospy.spin()
+    print("Arrived at waypoint!")
+
+    # Wait a small period of time before moving to the next waypoint
+    rospy.sleep(0.2)
