f7a5f4bec131c1a0d5cf6971b0ff65c6f4dc47d6
Kory Melchior <kory.melchior@dal.ca>
2022-11-14 16:25:37 -0400

    added features for waypoint navigation so "fast" and "stable" can be specified along with "passenger pickup" for an extra wait time at a waypoint.

--- waypoint_control/waypoint_navigation.py
+++ waypoint_control/waypoint_navigation.py
@@ -30,11 +30,30 @@ def arrived_at_waypoint(current, waypoint):
 
     return False
 
+def stable_at_waypoint(current, waypoint):
+    dx = abs(waypoint.position.x - current.position.x)
+    dy = abs(waypoint.position.y - current.position.y)
+    dz = abs(waypoint.position.z - current.position.z)
+
+    close_enough = 0.3
+
+    # If we're in this range
+    if dx < close_enough and dy < close_enough and dz < close_enough:
+        return True
+    
+    # give time to stabilize
+    rospy.sleep(0.7)
+
+    return False
+
 
 # Open Waypoints JSON File
-
 input_file =  input("Enter .json file name for waypoints: ")
-waypoint_file = open(os.path.dirname(__file__) + "/" + input_file + ".json")
+
+if not input_file.endswith('.json'):
+    input_file = input_file + '.json'
+    
+waypoint_file = open(os.path.dirname(__file__) + "/" + input_file)
 waypoints = json.load(waypoint_file)
 
 # Initialize ROS Functionality
@@ -46,6 +65,10 @@ rospy.Subscriber("/ground_truth_to_tf/pose", PoseStamped, gazebo_pose_callback)
 while(pose_pub.get_num_connections() < 1):
     pass
 
+# Waypoint settings set to defualts
+waypoint_type = "fast" 
+passenger_pickup = "no"
+
 for waypoint in waypoints:
     print("Moving to waypoint")
     print(waypoint)
@@ -53,15 +76,33 @@ for waypoint in waypoints:
     pose.position.x = waypoint["x"]
     pose.position.y = waypoint["y"]
     pose.position.z = waypoint["z"]
+    # New parameter for waypoint to sepecify if the drone should move through it quickly or take time to stabilize
+    try: 
+        waypoint_type = waypoint["type"]
+    except:
+        pass
+    # new parameter to specify if the drone should wait as if its picking up a passenger
+    try: 
+        passenger_pickup = waypoint["passenger pickup"]
+    except:
+        pass
 
     pose_msg = PoseStamped()
     pose_msg.pose = pose
 
     pose_pub.publish(pose_msg)
 
-    # Wait until waypoint has been reached
-    while(not arrived_at_waypoint(cur_pose, pose_msg.pose)):
-        pass
+    # Wait until waypoint has been reached. Move fast or stabilize depending on waypoint type
+    if waypoint_type == "fast":
+        while(not arrived_at_waypoint(cur_pose, pose_msg.pose)):
+            pass
+    elif waypoint_type == "stable":
+        while(not stable_at_waypoint(cur_pose, pose_msg.pose)):
+            pass
+
+    # check to see if theres a passenger pickup
+    if passenger_pickup == "yes":
+        rospy.sleep(4)
 
     print("Arrived at waypoint!")
 
